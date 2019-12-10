49ec5ae2bf53cefcb900c7f57d0aa4a4f3e05ab8
Liz P <parisielizabeth@gmail.com>
2019-12-10 03:11:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,8 +4,8 @@ class SongsController < ApplicationController
       @artist = Artist.find(id: params[:artist_id]).songs
       
       if @artist.nil? 
-         
-        flash[:alert] = "Artist not found"redirect to artists_path
+        flash[:alert] = "Artist not found"
+        redirect to artists_path
       else
         @songs = @artist.songs
       end 
